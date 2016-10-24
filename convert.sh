f776b3c612b1c08cfd22f6f196d83fb799513825
Steven Christe <steven.d.christe@nasa.gov>
2016-10-23 23:02:59 -0400

    creation of tool to make generated files

---
+++ convert.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import subprocess
+import glob
+import os
+from os.path import basename
+# Remove old files
+files = glob.glob('generated/*')
+for f in files:
+    os.remove(f)
+
+source_files = glob.glob('*.svg')
+print(source_files)
+# Convert form Inkscape SVG to PNG
+#inkscape sunpy_logo.svg --export-png=generated/sunpy_logo.png
+for f in source_files:
+    subprocess.call(["inkscape", f, "--export-png=generated/" + os.path.splitext(f)[0] + ".png"])
+
+# Convert form Inkscape SVG to Plain SVG (for web use)
+#inkscape sunpy_logo.svg --export-plain-svg=generated/sunpy_logo_plain.svg
+for f in source_files:
+    subprocess.call(["inkscape", f, "--export-plain-svg=generated/" + os.path.splitext(f)[0] + ".svg"])
+
+# Convert to PNGs required by Astropy website/docs
+#convert -intent Saturation -colorspace sRGB -filter Lanczos -resize 1000x96 -trim generated/sunpy_logo.png generated/sunpy_banner_96.png
+
+# Remove full-res PNGs since these aren't needed
+#rm generated/sunpy_logo.png
